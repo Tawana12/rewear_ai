1f35359252d598fe1e1b50f2aac6aabfc1d2c222
VisOna <makuvazatawa@gmail.com>
2026-01-30 16:49:58 +0000

    almost

--- rewear_ai/wardrobe/models.py
+++ rewear_ai/wardrobe/models.py
@@ -9,7 +9,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(256))
     
     # ROLE SYSTEM: 'user' for members, 'admin' for you
     role = db.Column(db.String(20), default='user') 
